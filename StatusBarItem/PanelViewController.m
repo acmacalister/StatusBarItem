9864b42322880bf742d7a61f35b020f489a28728
Austin Cherry <chillingwithim@gmail.com>
2014-04-16 22:08:59 -0700

    little bit of work

--- StatusBarItem/PanelViewController.m
+++ StatusBarItem/PanelViewController.m
@@ -11,18 +11,47 @@
 #import "PanelViewController.h"
 
 @interface PanelViewController ()
+@property (weak) IBOutlet NSView *processesView;
+@property (weak) IBOutlet NSView *portsView;
 
 @end
 
 @implementation PanelViewController
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Initialization code here.
+        [self processLoop];
+        [self portsLoop];
     }
     return self;
 }
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)processLoop
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), ^{
+        @autoreleasepool {
+            NSTask *task = [[NSTask alloc] init]; // ps -eo pcpu,pid,user,command | sort -k 1 -r | head -6
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // update the UI!!
+            });
+        }
+    });
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
+- (void)portsLoop
+{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0ul), ^{
+        @autoreleasepool {
+            NSTask *task = [[NSTask alloc] init]; // lsof -i tcp:0-1024
+            dispatch_async(dispatch_get_main_queue(), ^{
+                // update the UI!!
+            });
+        }
+    });
+}
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 @end
